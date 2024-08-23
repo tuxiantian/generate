6c9cb2026e13f66d5558a8c2f4f83eb3d2574129
tuxiantian <tuxiantian@163.com>
2024-08-23 18:12:12 +0800

    rocketmq的用法示例移到单独的包

--- src/main/java/com/tuxt/generate/rocketmq/TestController.java
+++ src/main/java/com/tuxt/generate/rocketmq/TestController.java
@@ -1,8 +1,5 @@
 package com.tuxt.generate.rocketmq;
 
-import com.tuxt.generate.workflow.MessageProducer;
-import com.tuxt.generate.workflow.WorkFlow;
-import com.tuxt.generate.workflow.WorkFlowEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.bind.annotation.GetMapping;
